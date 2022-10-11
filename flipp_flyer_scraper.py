69339252877fd52d02038425aacd8390807d6000
Caitlan Krasinski <ckrasinski@live.ca>
2022-10-10 20:27:47 -0400

    add comments

--- flipp_flyer_scraper.py
+++ flipp_flyer_scraper.py
@@ -14,6 +14,7 @@ start_time = time.time()
 
 print('process started')
 
+# update this link to wloo 
 link  = 'https://flipp.com/en-ca/guelph-on/flyer/5231790-freshco-weekly-eflyer-10061012?postal_code=N1E1A1'
 
 # SET OPTIONS 
@@ -29,8 +30,10 @@ scraper.nav_to_page(driver, link)
 
 time.sleep(4) # wait for page load 
 
+# find all item tags 
 flyer_items = driver.find_elements_by_class_name('item-container')
 
+# iterate through each <a> and get the item ids 
 items = []
 for item in flyer_items:
     itemid = item.get_attribute('itemid')
@@ -44,18 +47,21 @@ print('collecting item details')
 df = pd.DataFrame(columns = ['product_name', 'price'])
 
 for item in items:
+    # popup details for each item is located at this link 
     link = f'https://flipp.com/en-ca/guelph-on/item/{item}-freshco-weekly-eflyer-10061012?postal_code=N1E1A1'
     
     scraper.nav_to_page(driver, link)
     
     time.sleep(2)   
 
+    # collect item name and price - all name and pricing data will be left raw 
     name = driver.find_element_by_xpath('/html/body/flipp-dialog/div/flipp-toast-container/div/flipp-item-dialog/div/h2/span').text
     pre_price_text = driver.find_elements_by_class_name('pre-price-text')
     dollar = driver.find_elements_by_class_name('dollars')
     cents = driver.find_elements_by_class_name('cents')
     price_text = driver.find_elements_by_class_name('price-text')
 
+    # make sure each detail is present 
     if len(pre_price_text) > 0: pre_price_text_str= pre_price_text[0].text
     if len(dollar) > 0: dollar_str= dollar[0].text
     if len(cents) > 0: cents_str= cents[0].text
@@ -63,13 +69,14 @@ for item in items:
 
     price = f'{pre_price_text_str}${dollar_str}.{cents_str} {price_text_str}'
 
-     # append data to df 
+    # append data to df 
     df = df.append({'product_name': name, 
                     'price': price,}
                 , ignore_index=True)
 
+# save data 
 df.to_csv(f'csv_files/freshco/flyer_deals.csv', index=False)
 
 print(f'completed in {(time.time() - start_time)}')
 
-driver.close()
+driver.close() # close driver window 
