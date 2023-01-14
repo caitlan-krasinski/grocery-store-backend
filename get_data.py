205fbcb0725a65a2b8058404f3d714199ad1392c
Caitlan Krasinski <ckrasinski@live.ca>
2023-01-14 12:48:12 -0500

    uncomment

--- get_data.py
+++ get_data.py
@@ -1,6 +1,6 @@
 print('##################### PROCESS STARTED #####################')
 
-# import loblaw_co_scraper
+import loblaw_co_scraper
 import flipp_scraper 
 import clean_data 
 # ** add script to generate non loblaw co data here **
