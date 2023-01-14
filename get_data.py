6c4ea08f44afec4e654ceccd9f2ce15433e3a42d
Caitlan Krasinski <ckrasinski@live.ca>
2023-01-14 13:38:14 -0500

    update scraper

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 print('##################### PROCESS STARTED #####################')
 
 import loblaw_co_scraper
-import flipp_scraper 
+import flipp_scraper_v1
 import clean_data 
 # ** add script to generate non loblaw co data here **
 import build_index
